17be4319135c8eab3579ba39ec295bc85eb0ae43
Vladimir Yavdoshchuk <yavdoshchuk.vladimir@steelkiwi.com>
2019-12-04 12:16:04 +0200

    Add examples

--- example/app.js
+++ example/app.js
@@ -8,3 +8,16 @@ new ElementConnections({
     container: linesContainer,
     elements: [sq1, sq2, sq6, sq3]
 })
+
+const linesContainer2 = document.getElementById('connections2');
+const sq21 = document.getElementById('sq21')
+const sq22 = document.getElementById('sq22')
+
+new ElementConnections({
+    container: linesContainer2,
+    elements: [sq21, sq22],
+    style: {
+        width: 6,
+        color: 'blue'
+    }
+})
